40a645c97982958ce5ff601aafe5d0df6b85b806
JackBlackLight <jd2148@columbia.edu>
2018-11-26 12:07:08 -0500

    adds processor by search type conditional

--- lib/blacklight_internet_archive/entity_processor.rb
+++ lib/blacklight_internet_archive/entity_processor.rb
@@ -2,15 +2,14 @@ require 'active_support/core_ext/string/output_safety'
 require 'cgi'
 require_relative 'sites_entity_processor'
 
-
 module BlacklightInternetArchive
   # extract and convert individual results from response
   class EntityProcessor
 
   	def self.get_processor(search_type = 'catalog')
-  		if search_type == ''
+  		if search_type == 'search_pages'
   			return ArchivedPagesEntityProcessor.new
-  		elsif search_type == ''
+  		elsif search_type == 'search_videos'
         return SeedVideosEntityProcessor.new
       else 
         return SitesEntityProcessor.new
@@ -21,7 +20,5 @@ module BlacklightInternetArchive
     def run(response_json, base_url)
         raise NotImplementedError
     end
-
-
   end
 end
